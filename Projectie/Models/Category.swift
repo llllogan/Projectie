98eeac53d5286ecbe873c056cd1a27e24f73c316
logan <logan.janssen@icloud.com>
2025-02-17 11:51:28 +1000

    Updated the transaction category colours to separate money coming in from money going out

--- Projectie/Models/Category.swift
+++ Projectie/Models/Category.swift
@@ -18,90 +18,90 @@ let categories: [CategoryItem] = [
     CategoryItem(
         name: "Home",
         systemName: "house.fill",
-        color: .blue.opacity(0.7)),
+        color: .blue),
     CategoryItem(
         name: "Utilities",
         systemName: "lightbulb.fill",
-        color: .blue.opacity(0.7)),
+        color: .blue),
     CategoryItem(
         name: "Maintenance",
         systemName: "wrench.and.screwdriver.fill",
-        color: .blue.opacity(0.7)),
+        color: .blue),
     CategoryItem(
         name: "Bill",
         systemName: "text.document.fill",
-        color: .blue.opacity(0.7)),
+        color: .blue),
 
     
     
     CategoryItem(
         name: "Groceries",
         systemName: "cart.fill",
-        color: .green.opacity(0.7)),
+        color: .green),
     
     CategoryItem(
         name: "Health",
         systemName: "heart.fill",
-        color: .green.opacity(0.7)),
+        color: .green),
     CategoryItem(
         name: "Medical",
         systemName: "stethoscope",
-        color: .green.opacity(0.7)),
+        color: .green),
     CategoryItem(
         name: "Self Care",
         systemName: "scissors",
-        color: .green.opacity(0.7)),
+        color: .green),
     
     
     CategoryItem(
         name: "Transport",
         systemName: "bus",
-        color: .brown.opacity(0.7)),
+        color: .brown),
 
     
     CategoryItem(
         name: "Personal",
         systemName: "person.fill",
-        color: .orange.opacity(0.7)),
+        color: .orange),
     CategoryItem(
         name: "Shopping",
         systemName: "bag.fill",
-        color: .orange.opacity(0.7)),
+        color: .orange),
     
     
     CategoryItem(
         name: "Transfer",
         systemName: "arrow.left.arrow.right.square.fill",
-        color: .pink.opacity(0.7)),
+        color: .pink),
     CategoryItem(
         name: "Payment",
         systemName: "arrowshape.turn.up.right.fill",
-        color: .pink.opacity(0.7)),
+        color: .pink),
 
     CategoryItem(
         name: "Income",
         systemName: "banknote.fill",
-        color: .pink.opacity(0.7)),
+        color: Color(hue: 97/360, saturation: 0.9, brightness: 0.77)),
     CategoryItem(
         name: "Savings",
         systemName: "dollarsign.bank.building.fill",
-        color: .pink.opacity(0.7)),
+        color: Color(hue: 97/360, saturation: 0.9, brightness: 0.77)),
     
     
     CategoryItem(
         name: "Sport",
         systemName: "american.football.fill",
-        color: .indigo.opacity(0.7)),
+        color: .indigo),
     CategoryItem(
         name: "Entertainment",
         systemName: "ticket.fill",
-        color: .indigo.opacity(0.7)),
+        color: .indigo),
     
     
     CategoryItem(
         name: "None",
         systemName: "circle.dashed",
-        color: .black.opacity(0.7)),
+        color: .black),
 ]
 
 func findCategoryBySystemName(_ systemName: String) -> CategoryItem {
